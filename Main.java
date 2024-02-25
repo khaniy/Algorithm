8a5135e76f96bda30a04f4066dfdb069c65d187a
hank0302 <kangh1596@gmail.com>
2024-02-26 00:19:01 +0900

    [23/02/25] 백준_2562번, 최댓값

--- Main.java
+++ Main.java
@@ -4,18 +4,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-
-        String[] intput = br.readLine().split(" ");
+        int N = 9;
         int[] st = new int[N];
-        for (int i = 0; i < N; i++) {st[i] = Integer.parseInt(intput[i]);}
-        int max = st[0];
-        int min = st[0];
-        for (int i =1; i< st.length; i++){
-            if(max<st[i]){max = st[i];}
-            if(min>st[i]){min = st[i];}
+        for (int i = 0; i < N; i++) {
+            st[i] = Integer.parseInt(br.readLine());
         }
-        bw.write(String.valueOf(min) + " " + String.valueOf(max));
+        int max = st[0];
+        int loc = 1;
+        for (int i =1; i< st.length; i++){
+            if(max <= st[i]){
+                max = st[i];
+                loc = i+1;
+            }
+        }
+        bw.write(String.valueOf(max) + " " + String.valueOf(loc));
 
         bw.flush();
         br.close();
