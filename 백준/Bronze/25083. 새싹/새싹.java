59ffdf334bed465a4c3aec7cf0e1bac12d71f9fc
hank0302 <111636869+hank0302@users.noreply.github.com>
2024-02-27 13:38:44 +0900

    [Bronze V] Title: 새싹, Time: 172 ms, Memory: 16424 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(
+                "         ,r'\"7" + System.lineSeparator()
+                + "r`-_   ,'  ,/" + System.lineSeparator()
+                + " \\. \". L_r'" + System.lineSeparator()
+                + "   `~\\/" + System.lineSeparator()
+                + "      |" + System.lineSeparator()
+                + "      |");
+
+    }
+
+}
