7e98b661454682b39eed0360ad1a68e17b960ba3
hank0302 <111636869+hank0302@users.noreply.github.com>
2024-02-27 16:17:49 +0900

    [Bronze I] Title: 단어 공부, Time: 648 ms, Memory: 125680 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] input = br.readLine().split("");
+        HashMap<String, Integer> store = new HashMap<>();
+        for (String value : input) {
+            String word = value.toUpperCase();
+            if (store.containsKey(word)) {
+                store.merge(word, 1, Integer::sum);
+            } else {
+                store.put(word, 1);
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        String maxKey = null;
+        for (Map.Entry<String, Integer> entry : store.entrySet()) {
+            if (entry.getValue() > max) {
+                max = entry.getValue();
+                maxKey = entry.getKey();
+            }
+            else if (entry.getValue() == max && !entry.getKey().equals(maxKey)) {
+                maxKey = "?";
+            }
+        }
+        bw.write(maxKey);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
