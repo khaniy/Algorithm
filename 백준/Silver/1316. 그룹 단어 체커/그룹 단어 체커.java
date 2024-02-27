103d5f9eb3277e4fa01cec0d063402f7f0107859
hank0302 <111636869+hank0302@users.noreply.github.com>
2024-02-27 23:35:12 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        for(int i=0; i<N; i++){
+            String input = br.readLine();
+            if(isGroupWord(input)){
+                count+=1;
+            }
+        }
+        bw.write(String.valueOf(count));
+        bw.close();
+        br.close();
+
+    }
+
+    private static boolean isGroupWord(String input){
+        boolean[] check = new boolean[26];
+        for(int j=0; j<input.length(); j++){
+            char now = input.charAt(j);
+            char f = (j==0) ? input.charAt(0) : input.charAt(j - 1);
+            if (check[now-'a']){
+                if(now != f){
+                    return false;
+                }
+            }else{
+                check[now-'a'] = true;
+            }
+        }
+
+        return true;
+    }
+}
