f596d25d9db1b129ba9d0cd9b79453453d694ade
hank0302 <kangh1596@gmail.com>
2024-02-27 23:36:57 +0900

    [2024/02/27] 복잡한 수식은 함수로 빼서 다시 생각해보기...

--- 백준/Solution/Main.java
+++ 백준/Solution/Main.java
@@ -1,29 +1,14 @@
+import solution.S1316;
+
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        Solver solver = new Solver(new S1157());
-//        solver.solve();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        int count = 0;
-        for(int i=0; i<N; i++){
-            String input = br.readLine();
-            String s = null;
-            boolean seq = false;
-            for(int j=0; j<input.length(); j++){
-                String c = Character.toString(input.charAt(j));
-                if(!s.equals(c)){
-                    s=c;
+        Solver solver = new Solver(new S1316());
+        solver.solve();
 
-                }
-                if(s.contains(c)){
-
-                }
-
-            }
-        }
 
     }
+
+
 }
--- 백준/Solution/solution/S1316.java
+++ 백준/Solution/solution/S1316.java
@@ -7,15 +7,32 @@ public class S1316 implements Solution {
     public void solve() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String str = br.readLine();
-        String[] Croatia = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
-        for (String value : Croatia) {
-            str = str.replace(value, "q");
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        for(int i=0; i<N; i++){
+            String input = br.readLine();
+            if(isGroupWord(input)){
+                count+=1;
+            }
         }
-        bw.write(String.valueOf(str.length()));
-
-        bw.flush();
+        bw.write(String.valueOf(count));
         bw.close();
         br.close();
+    }
+    private static boolean isGroupWord(String input){
+        boolean[] check = new boolean[26];
+        for(int j=0; j<input.length(); j++){
+            char now = input.charAt(j);
+            char f = (j==0) ? input.charAt(0) : input.charAt(j - 1);
+            if (check[now-'a']){
+                if(now != f){
+                    return false;
+                }
+            }else{
+                check[now-'a'] = true;
+            }
+        }
+
+        return true;
     }
 }
