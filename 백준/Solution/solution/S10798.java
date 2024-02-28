b6d8dd901dd638303a4d2a72b25a625ee4682b03
khaniy <kangh1596@gmail.com>
2024-02-28 23:12:16 +0900

    [2024/02/28] 문제를 잘 읽고 꼼수로 해결

---
+++ 백준/Solution/solution/S10798.java
@@ -0,0 +1,26 @@
+package solution;
+
+import java.io.*;
+
+public class S10798 implements Solution {
+    @Override
+    public void solve() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[][] storage = new String[5][];
+        for (int i=0; i<5; i++){
+            String[] split = br.readLine().split("");
+            storage[i] = split;
+        }
+        for(int j=0; j<15; j++){
+            for(int i=0; i<5; i++){
+                try{
+                    bw.write(storage[i][j]);
+                }catch (Exception e){
+                }
+            }
+        }
+        bw.close();
+        br.close();
+    }
+}
