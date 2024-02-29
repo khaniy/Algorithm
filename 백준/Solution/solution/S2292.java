7a82215c7a1227609d0b7e69c4ca45f6d6f8e11c
khaniy <kangh1596@gmail.com>
2024-02-29 20:59:27 +0900

    [2024/02/29] 잊고있었던 수열,,,

--- 백준/Solution/solution/S2292.java
+++ 백준/Solution/solution/S2292.java
@@ -1,21 +1,22 @@
+package solution;
+
 import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-//        Solver solver = new Solver(new S2738());
-//        solver.solve();
+public class S2292 implements Solution {
+    @Override
+    public void solve() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
         int l = 0;
         int start = 0;
         while(true){
-          int last= (int) (Math.pow(l,2) * 3 + 3*l + 1);
-          if (start < N && N <= last){
-              break;
-          }
-          l+=1;
-          start = last;
+            int last= (int) (Math.pow(l,2) * 3 + 3*l + 1);
+            if (start < N && N <= last){
+                break;
+            }
+            l+=1;
+            start = last;
         }
         bw.write(String.valueOf(l+1));
         bw.close();
