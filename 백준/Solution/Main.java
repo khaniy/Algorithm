93edc594c1ef4e24c90a49519db47d1382bdbb7e
khaniy <kangh1596@gmail.com>
2024-02-29 18:01:21 +0900

    [2024/02/27] 11005푸는 ...중

--- 백준/Solution/Main.java
+++ 백준/Solution/Main.java
@@ -6,6 +6,26 @@ public class Main {
 //        solver.solve();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+        StringBuilder sb = new StringBuilder();
+        while (true){
+            N = N / B;
+            int rest = N % B;
+            if(10 <= rest){
+                 sb.append((char) ('A' + rest-10));
+            }else{
+                sb.append(String.valueOf(rest));
+            }
+            if (N == 0){
+                break;
+            }
+
+        }
+        System.out.println(35/35);
+        System.out.println(35%35);
+        bw.write(sb.reverse().toString());
         bw.close();
         br.close();
     }
