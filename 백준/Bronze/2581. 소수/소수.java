a99d2e7b3e3badab2da88c4d10588ba5209fac16
Kang Han <111636869+khaniy@users.noreply.github.com>
2024-03-01 12:33:54 +0900

    [Bronze II] Title: 소수, Time: 336 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        int sum = 0;
+        int first = 0;
+        boolean isFirst = true;
+        for (int i = M; i <= N; i++) {
+            if (isPrimary(i)) {
+                sum += i;
+                if(isFirst){
+                    first = i;
+                    isFirst = false;
+                }
+            }
+        }
+        if(sum !=0){
+            System.out.println(sum);
+            System.out.println(first);
+        }else {
+            System.out.println(-1);
+        }
+    }
+
+    private static boolean isPrimary(int i) {
+        int count = 0;
+        for (int j = 1; j <= i; j++) {
+            if (i % j == 0) {
+                count += 1;
+            }
+        }
+        if (count == 2) {
+            return true;
+        }
+        return false;
+    }
+
+
+}
