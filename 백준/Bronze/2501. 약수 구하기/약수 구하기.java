b2c6c52a09fce2fca5d84797d2899d835fc0cd66
Kang Han <111636869+khaniy@users.noreply.github.com>
2024-03-01 10:36:10 +0900

    [Bronze III] Title: 약수 구하기, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
+        ArrayList<Integer> divisors = new ArrayList<>();
+        int count = 0;
+        for (int i = 1; i <= N; i++) {
+            if (N % i == 0) {
+                count++;
+                if (count == K) {
+                    System.out.println(i);
+                    return;
+                }
+            }
+        }
+        System.out.println(0);
+        br.close();
+    }
+
+}
