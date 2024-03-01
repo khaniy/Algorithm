76158bbbc43b229c076ce3b6185c02d6d030ccd2
Kang Han <111636869+khaniy@users.noreply.github.com>
2024-03-01 12:17:17 +0900

    [Bronze I] Title: 약수들의 합, Time: 152 ms, Memory: 15368 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+            int sum = 0;
+            StringBuilder sb = new StringBuilder();
+            ArrayList<Integer> store = new ArrayList<>();
+            if (n == -1) {
+                break;
+            }
+            for (int i = 1; i < n; i++) {
+                if (n % i == 0) {
+                    sum+=i;
+                    store.add(i);
+                }
+            }
+            Object[] array = store.toArray();
+            if(sum == n){
+                sb.append(n+ " = ");
+                for (int i=0; i<array.length; i++){
+                    sb.append(array[i]);
+                    if(i+1!= array.length){
+                        sb.append(" + ");
+                    }
+                }
+                System.out.println(sb.toString());
+            }else {
+                sb.append(n + " is NOT perfect.");
+                System.out.println(sb.toString());
+            }
+        }
+        br.close();
+    }
+
+
+}
