912c964d5c622572397fee779d157677064fcbf9
khaniy <kangh1596@gmail.com>
2024-03-04 00:33:37 +0900

    [2024/03/04] Switch문 사용

--- 백준/Solution/Main.java
+++ 백준/Solution/Main.java
@@ -1,21 +1,13 @@
+import solution.S2738;
+
 import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        Solver solver = new Solver(new S2738());
-//        solver.solve();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int H = Integer.parseInt(st.nextToken());
-        int W = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int x = H % (N + 1) == 0 ? H / (N + 1) : H / (N + 1) + 1;
-        int y = W % (M + 1) == 0 ? W / (M + 1) : W / (M + 1) + 1;
-        bw.write(String.valueOf(x * y));
-        bw.close();
-        br.close();
+        Solver solver = new Solver(new S2738());
+        solver.solve();
     }
 }
