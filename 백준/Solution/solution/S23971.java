321ede164ab56a22fdb75245625184dbbd0d4163
khaniy <kangh1596@gmail.com>
2024-03-04 00:10:23 +0900

    [2024/03/04] 다르게 생각하기

--- 백준/Solution/solution/S23971.java
+++ 백준/Solution/solution/S23971.java
@@ -1,10 +1,13 @@
+package solution;
+
 import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-//        Solver solver = new Solver(new S2738());
-//        solver.solve();
+public class S23971 implements Solution {
+    @Override
+    public void solve() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
