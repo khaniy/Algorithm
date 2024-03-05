9418c989a272530ec39e7c2b411e5eef6481c5b4
khaniy <kangh1596@gmail.com>
2024-03-05 17:11:52 +0900

    [2024/03/05] 8979 푸는중...

--- 백준/Solution/Main.java
+++ 백준/Solution/Main.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -7,33 +9,16 @@ public class Main {
 //        solver.solve();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int T = Integer.parseInt(br.readLine());
-
-        for (int i = 1; i <= T; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int number = Integer.parseInt(st.nextToken());
-            int count = 0;
-            int[] store = new int[20];
-            for (int j = 0; j < 20; j++) {
-                int student = Integer.parseInt(st.nextToken());
-                count += caculate(student, store);
-                store[j] = student;
-            }
-            bw.write(String.valueOf(i)+" "+String.valueOf(count));
-            bw.newLine();
-        }
-        bw.close();
-        br.close();
-
-    }
-
-    private static int caculate(int student, int[] store) {
-        int count = 0;
-        for (int i = 0; i < store.length; i++) {
-            if (student < store[i]){
-                count+=1;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        ArrayList<List<Integer>> store = new ArrayList<>();
+        for (int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j=0; j<4; j++){
+                Integer.parseInt(st.nextToken())
+                store.add()
             }
         }
-        return count;
     }
 }
