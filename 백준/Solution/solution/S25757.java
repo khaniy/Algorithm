b873b312f01a3b9002af0d35f70407c1734caa14
khaniy <kangh1596@gmail.com>
2024-03-10 18:07:46 +0900

    [2024/03/10] - 복잡하게 생각하지 말자...

---
+++ 백준/Solution/solution/S25757.java
@@ -0,0 +1,29 @@
+package solution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class S25757 implements Solution {
+    @Override
+    public void solve() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        Map<String, Integer> miniGame = Map.ofEntries(
+                Map.entry("Y", 2),
+                Map.entry("F", 3),
+                Map.entry("O", 4)
+        );
+        st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int numOfGame = miniGame.get(st.nextToken());
+        Set<String> storage = new HashSet<String>();
+        for (int i = 0; i < N; i++) {
+            String Id = br.readLine();
+            storage.add(Id);
+        }
+        int people = storage.toArray().length;
+        System.out.println(people/(numOfGame-1));
+    }
+}
