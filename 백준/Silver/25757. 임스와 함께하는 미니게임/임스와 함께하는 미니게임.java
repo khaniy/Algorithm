3d3278427df4d156394947408ff5b858acf251ba
Kang Han <111636869+khaniy@users.noreply.github.com>
2024-03-10 18:06:37 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 280 ms, Memory: 27548 KB -BaekjoonHub

---
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        Map<String, Integer> miniGame = Map.ofEntries(
+                Map.entry("Y", 2),
+                Map.entry("F", 3),
+                Map.entry("O", 4)
+        );
+        st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int numOfGame = miniGame.get(st.nextToken());
+        Set<String> storage = new HashSet<String>();
+        for (int i = 0; i < N; i++) {
+            String Id = br.readLine();
+            storage.add(Id);
+        }
+        int people = storage.toArray().length;
+        System.out.println(people/(numOfGame-1));
+    }
+}
