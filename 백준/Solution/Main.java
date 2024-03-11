d33eac865ca23c4b8df82f46908904b34ec237d2
khaniy <kangh1596@gmail.com>
2024-03-11 10:40:40 +0900

    [2024/03/11] 함수로 쪼개고 문제 제대로 읽기

--- 백준/Solution/Main.java
+++ 백준/Solution/Main.java
@@ -1,37 +1,65 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int N = scanner.nextInt();
-        int newScore = scanner.nextInt();
-        int P = scanner.nextInt();
-        int[] scores = new int[N];
-
-        for (int i = 0; i < N; i++) {
-            scores[i] = scanner.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] switchList = new int[n];
+        for (int i = 0; i < n; i++) {
+            switchList[i] = sc.nextInt();
         }
-        int rank = findRank(scores, N, newScore, P);
-        System.out.println(rank);
+        int studentNumber = sc.nextInt();
+        for (int i = 0; i < studentNumber; i++) {
+            int sex = sc.nextInt();
+            int loc = sc.nextInt();
+            changeSwitch(switchList, sex, loc);
+        }
+
+        printList(switchList);
+
     }
 
-    public static int findRank(int[] scores, int N, int newScore, int P) {
+    private static void printList(int[] a) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < a.length; i++) {
 
-        if (N < P || (N == P && newScore > scores[N - 1])) {
-            if (N == 0 || newScore > scores[0]) {
-                return 1;
-            } else {
-                for (int i = 0; i < N; i++) {
-                    if (newScore >= scores[i]) {
-                        return i + 1;
-                    }
-                }
-                return N + 1;
+            if (i % 20 == 0 && i != 0) {
+                sb.append(System.lineSeparator());
             }
-        } else {
-            return -1;
+            sb.append(a[i]);
+            sb.append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+
+
+    private static void changeSwitch(int[] a, int sex, int loc) {
+        if (sex == 1) {
+            for (int i = loc - 1; i < a.length; i += loc) {
+                change(a, i);
+
+            }
+        } else if (sex == 2) {
+            change(a, loc - 1);
+            for (int gap = 1; loc + gap - 1 < a.length && loc - 1 - gap >= 0; gap++) {
+                if (isSymmetry(a, loc - 1, gap)) {
+                    change(a, loc + gap - 1);
+                    change(a, loc - gap - 1);
+                } else
+                    break;
+            }
         }
     }
+
+    private static boolean isSymmetry(int[] a, int loc, int gap) {
+        return a[loc + gap] == a[loc - gap];
+    }
+
+    private static void change(int[] a, int loc) {
+        if (a[loc] == 1)
+            a[loc] = 0;
+        else if (a[loc] == 0)
+            a[loc] = 1;
+    }
+
 }
---
+++ 백준/Solution/solution/S4659.java
@@ -0,0 +1,73 @@
+package solution;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class S4659 implements Solution {
+    @Override
+    public void solve() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        char[] vowels = new char[]{'a', 'e', 'i', 'o', 'u'};
+        while (true) {
+            String input = br.readLine();
+            if (input.equals("end")) {
+                break;
+            }
+            if (isAcceptable(input)) {
+                bw.write("<" + input + ">" + " is acceptable.");
+            } else {
+                bw.write("<" + input + ">" + " is not acceptable.");
+            }
+            bw.newLine();
+        }
+        bw.close();
+        br.close();
+    }
+
+    private static boolean isVowel(char c) {
+        return "aeiou".indexOf(c) != -1;
+    }
+
+    private static boolean isConsecutive(char f, char b) {
+        return f == b;
+    }
+
+    private static boolean isAcceptable(String password) {
+        boolean isVowelWord = false;
+        int consecutiveCount = 1;
+        int vowelCount = 0;
+        int consonantCount = 0;
+        char lastChar = ' ';
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+            if (isVowel(c)) {
+                isVowelWord = true;
+                vowelCount += 1;
+                consonantCount = 0;
+                if (isConsecutive(c, lastChar)) {
+                    if (c != 'e' && c != 'o') {
+                        return false;
+                    }
+                }
+                if (vowelCount > 2) {
+                    return false;
+                }
+            } else {
+                vowelCount = 0;
+                consonantCount += 1;
+                if (consonantCount > 2) {
+                    return false;
+                }
+                if (isConsecutive(c, lastChar)) {
+                    return false;
+                }
+            }
+            lastChar = c;
+        }
+        if (!isVowelWord) {
+            return false;
+        }
+        return true;
+    }
+}
