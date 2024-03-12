d4a4185b6e7e3fc36fcac7cf861b5828354cb966
Kang Han <111636869+khaniy@users.noreply.github.com>
2024-03-12 15:38:09 +0900

    [Silver III] Title: 크로스 컨트리, Time: 476 ms, Memory: 32056 KB -BaekjoonHub

---
+++ 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        for (int testCase = 0; testCase < T; testCase++) {
+            int N = scanner.nextInt();
+            int[] runners = new int[N];
+            HashMap<Integer, ArrayList<Integer>> store = new HashMap<>();
+            for (int i = 0; i < N; i++) {
+                int team = scanner.nextInt();
+                runners[i] = team;
+                store.computeIfAbsent(runners[i], k -> new ArrayList<>()).add(i + 1);
+            }
+            ArrayList<Integer> exceptTeams = new ArrayList<Integer>();
+            for (Map.Entry<Integer, ArrayList<Integer>> entry : store.entrySet()) {
+                if (entry.getValue().toArray().length < 6) {
+                    exceptTeams.add(entry.getKey());
+                }
+            }
+            HashMap<Integer, ArrayList<Integer>> finalStore = new HashMap<>();
+            ArrayList<Integer> finalRunners = new ArrayList<>();
+            for (Integer team : runners) {
+                if (!exceptTeams.contains(team)) {
+                    finalRunners.add(team);
+                }
+            }
+            for (int i = 0; i < finalRunners.size(); i++) {
+                finalStore.computeIfAbsent(finalRunners.get(i), k -> new ArrayList<>()).add(i + 1);
+            }
+            int winningTeam = -1;
+            int lowestScore = Integer.MAX_VALUE;
+            int fifthScore = Integer.MAX_VALUE;
+            for (Map.Entry<Integer, ArrayList<Integer>> entry : finalStore.entrySet()) {
+                ArrayList<Integer> positions = entry.getValue();
+                Collections.sort(positions);
+                int score = positions.get(0) + positions.get(1) + positions.get(2) + positions.get(3);
+                if (score < lowestScore) {
+                    winningTeam = entry.getKey();
+                    lowestScore = score;
+                    fifthScore = positions.get(4);
+                } else if (score == lowestScore && positions.get(4) < fifthScore) {
+                    winningTeam = entry.getKey();
+                    fifthScore = positions.get(4);
+                }
+            }
+            System.out.println(winningTeam);
+        }
+        scanner.close();
+    }
+}
