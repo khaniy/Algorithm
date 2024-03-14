ba30635f77571e3b28f71a017ae36839aa54fd17
khaniy <kangh1596@gmail.com>
2024-03-14 10:21:48 +0900

    [2024/03/14] 객체 지향적인 생각

--- 백준/Solution/solution/S20125.java
+++ 백준/Solution/solution/S20125.java
@@ -1,7 +1,13 @@
-import java.io.*;
+package solution;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class S20125 implements Solution {
+    @Override
+    public void solve() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         Cookie cookie = new Cookie();
