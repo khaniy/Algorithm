ffdd356b86188f4a68eb3b5a7c452a7a133ed049
khaniy <kangh1596@gmail.com>
2024-03-15 10:19:29 +0900

    [2024/03/15] 조건을 잘 보고 데이터 타입 명시할 것

--- 백준/Solution/solution/S13305.java
+++ 백준/Solution/solution/S13305.java
@@ -1,22 +1,26 @@
-import java.io.*;
+package solution;
+
+import java.io.IOException;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class S13305 implements Solution {
+    @Override
+    public void solve() throws IOException {
+
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        Long[] roadList = new Long[N-1];
+        Long[] roadList = new Long[N - 1];
         Long[] priceList = new Long[N];
-        for (int i=0; i<N-1; i++){
+        for (int i = 0; i < N - 1; i++) {
             roadList[i] = sc.nextLong();
         }
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             priceList[i] = sc.nextLong();
         }
         Long minPrice = Long.MAX_VALUE;
         long amt = 0;
-        for(int i=0; i<priceList.length-1; i++){
-            if(priceList[i]<minPrice)
+        for (int i = 0; i < priceList.length - 1; i++) {
+            if (priceList[i] < minPrice)
                 minPrice = priceList[i];
             amt += (long) minPrice * roadList[i];
 
