5cf44c16d989fe037216e4ae37a5710119a12694
khaniy <kangh1596@gmail.com>
2024-03-17 11:28:38 +0900

    [2024/03/17] - 이진탐색으로 풀기

--- 백준/Solution/solution/S2512.java
+++ 백준/Solution/solution/S2512.java
@@ -1,8 +1,15 @@
-import java.io.*;
-import java.util.*;
+package solution;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class S2512 implements Solution {
+    @Override
+    public void solve() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
