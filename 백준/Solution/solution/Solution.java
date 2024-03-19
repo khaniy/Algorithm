664ab667f00d6c2faa6ef5566064d4e300af892b
khaniy <kangh1596@gmail.com>
2024-03-19 09:17:57 +0900

    [2024/03/19] 문제풀이 포맷 -> given / when / then 으로 변경

---
+++ 백준/Solution/solution/Solution.java
@@ -0,0 +1,9 @@
+package solution;
+
+import java.io.IOException;
+
+public interface Solution {
+    public void given() throws IOException;
+    public void when() throws IOException;
+    public void then() throws IOException;
+}
